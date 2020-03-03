d2891041fbf4b182f22f5f1f1574e4312fb7ba6e
Samy Ayoub <samyayoub@gmail.com>
2020-03-03 00:17:42 -0600

    Initial Commit with the first part of the project - Setup Boards/Lists/Cards

---
+++ assets/js/board.js
@@ -0,0 +1,49 @@
+class Card {
+	constructor(text) {
+		this.text = text;
+		this.id = ++Card.lastId;
+	}
+}
+
+Card.lastId = 0;
+
+class List {
+	constructor(title) {
+		this.title = title;
+		this.id = ++List.lastId;
+		this.cards = [];
+	}
+
+	addCard(text) {
+		var card = new Card(text);
+		this.cards.push(card);
+	}
+}
+
+List.lastId = 0;
+
+class Board {
+	constructor() {
+		this.id = ++Board.lastId;
+		this.lists = [];
+	}
+
+	addList(text) {
+		var list = new List(text);
+		this.lists.push(list);
+	}
+
+	addCard(listId, cardText) {
+		this.lists.forEach(function(list) {
+			if (listId === list.id) {
+				list.addCard(cardText);
+			}
+		});
+	}
+}
+
+Board.lastId = 0;
+
+var board = new Board("My First Board");
+board.addList("My First List");
+board.lists[0].addCard("My First Card");
